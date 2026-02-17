fba8de2e2d4e097a0cbe185d48eaa56fa2dfc173
Theebiga <theebivicky03@gmail.com>
2026-02-17 14:54:17 +0530

    package install swagger

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,6 +1,9 @@
+using Sample.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IProductService, ProductService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
