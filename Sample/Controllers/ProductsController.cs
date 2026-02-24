85add025bfd6a2132c4d88124aaeed4cab24c1b4
Theebiga <theebivicky03@gmail.com>
2026-02-24 22:43:31 +0530

    update controller and service

--- Sample/Controllers/ProductsController.cs
+++ Sample/Controllers/ProductsController.cs
@@ -1,86 +1,98 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Sample.Interfaces;
-using Sample.Models; 
-using System.Collections.Generic; 
+using Sample.Models;
+using System.Collections.Generic;
 
 namespace Sample.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly IProductService _productService;
+        private readonly IProductService productService;
+
         public ProductsController(IProductService productService)
         {
-            _productService = productService;
+            this.productService = productService;
         }
 
         [HttpGet]
         public IActionResult GetAll()
         {
-            try {
-                return Ok(_productService.GetAll());
+            try
+            {
+                return Ok(productService.GetAll());
             }
-            catch (Exception ex){
+            catch (Exception _)
+            {
                 return StatusCode(500, "Internal server error");
             }
         }
 
-        [HttpGet("{id}")] 
+        [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
-            try {
-                var product = _productService.GetById(id);
-                if (product == null)
+            try
             {
-                return NotFound(new { message = "Product not found!" });
+                var product = productService.GetById(id);
+                if (product == null)
+                {
+                    return NotFound(new { message = "Product not found!" });
+                }
+                return Ok(product);
             }
-            return Ok(product);
-            }
-            catch (Exception ex){
+            catch (Exception _)
+            {
                 return StatusCode(500, "Internal server error");
             }
         }
 
         [HttpPost]
-        public IActionResult Create(Product newProduct){
-
-            try {
-                var created = _productService.Create(newProduct);
-                return CreatedAtAction(nameof(GetById), new { id = newProduct.Id }, newProduct);
+        public IActionResult Create(Product newProduct)
+        {
+            try
+            {
+                var created = productService.Create(newProduct);
+                return CreatedAtAction(nameof(GetById), new { id = created.Id }, created);
             }
-            catch (Exception ex){
+            catch (Exception _)
+            {
                 return StatusCode(500, "Internal server error");
             }
         }
 
         [HttpPut("{id}")]
-        public IActionResult Update(int id, Product updateProduct){
-            try {
-                var updated = _productService.Update(id, updateProduct);
+        public IActionResult Update(int id, Product updateProduct)
+        {
+            try
+            {
+                var updated = productService.Update(id, updateProduct);
                 if (updated == null)
                     return NotFound(new { message = "Cannot Update, Product not Found" });
 
                 return NoContent();
             }
-            catch (Exception ex){
+            catch (Exception _)
+            {
                 return StatusCode(500, "Internal server error");
             }
         }
 
         [HttpDelete("{id}")]
-        public IActionResult Delete(int id){
-            try {
-                var deleted = _productService.Delete(id);
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var deleted = productService.Delete(id);
                 if (!deleted)
                     return NotFound(new { message = "Cannot Delete, Product not Found" });
 
                 return NoContent();
             }
-            catch (Exception ex){
+            catch (Exception _)
+            {
                 return StatusCode(500, "Internal server error");
             }
         }
-
     }
 }
