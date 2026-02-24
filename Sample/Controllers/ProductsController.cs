7a646e3104e0e41a2481ef1661c751ddfd7f1842
Theebiga <theebivicky03@gmail.com>
2026-02-24 22:37:31 +0530

    update controller and service

--- Sample/Controllers/ProductsController.cs
+++ Sample/Controllers/ProductsController.cs
@@ -1,46 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Sample.Interfaces;
-using Sample.Models; 
-using System.Collections.Generic; 
+using Sample.Models;
+using System.Collections.Generic;
 
 namespace Sample.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    public class ProductsController(IProductService productService) : ControllerBase
     {
-        private readonly IProductService _productService;
-        public ProductsController(IProductService productService)
-        {
-            _productService = productService;
-        }
+        private readonly IProductService productService = productService;
 
         [HttpGet]
         public IActionResult GetAll()
         {
-            return Ok(_productService.GetAll());
+            return Ok(productService.GetAll());
         }
 
-        [HttpGet("{id}")] 
+        [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
-            var product = _productService.GetById(id);
+            var product = productService.GetById(id);
             if (product == null)
-        {
-            return NotFound(new { message = "Product not found!" });
-        }
-        return Ok(product);
+            {
+                return NotFound(new { message = "Product not found!" });
+            }
+            return Ok(product);
         }
 
         [HttpPost]
-        public IActionResult Create(Product newProduct){
-            var created = _productService.Create(newProduct);
-            return CreatedAtAction(nameof(GetById), new { id = newProduct.Id }, newProduct);
+        public IActionResult Create(Product newProduct)
+        {
+            var created = productService.Create(newProduct);
+            return CreatedAtAction(nameof(GetById), new { id = created.Id }, created);
         }
 
         [HttpPut("{id}")]
-        public IActionResult Update(int id, Product updateProduct){
-            var updated = _productService.Update(id, updateProduct);
+        public IActionResult Update(int id, Product updateProduct)
+        {
+            var updated = productService.Update(id, updateProduct);
             if (updated == null)
                 return NotFound(new { message = "Cannot Update, Product not Found" });
 
@@ -48,13 +46,13 @@ namespace Sample.Controllers
         }
 
         [HttpDelete("{id}")]
-        public IActionResult Delete(int id){
-            var deleted = _productService.Delete(id);
+        public IActionResult Delete(int id)
+        {
+            var deleted = productService.Delete(id);
             if (!deleted)
                 return NotFound(new { message = "Cannot Delete, Product not Found" });
 
             return NoContent();
         }
-
     }
 }
